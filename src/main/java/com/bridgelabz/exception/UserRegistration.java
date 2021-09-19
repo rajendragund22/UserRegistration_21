a669933bc3aa83f10dfd36e47a5e5f780f62e3d8
RajendraGund <rajendragund2015@gmail.com>
2021-09-19 06:16:43 +0530

    Added : Welcome message for the user

---
+++ src/main/java/com/bridgelabz/exception/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.exception;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration code using exception!!");
+    }
+}
