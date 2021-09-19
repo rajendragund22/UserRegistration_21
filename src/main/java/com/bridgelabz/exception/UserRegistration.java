108737bf165138f4353889c882d644b89c11f1aa
RajendraGund <rajendragund2015@gmail.com>
2021-09-20 02:33:17 +0530

    Added : Program for the Passward is in format or not for Rule 1 Using Test Case

--- src/main/java/com/bridgelabz/exception/UserRegistration.java
+++ src/main/java/com/bridgelabz/exception/UserRegistration.java
@@ -28,4 +28,10 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
+    public boolean passwordRule1(String password) {
+        String regex = "^[0-9a-zA-Z]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
