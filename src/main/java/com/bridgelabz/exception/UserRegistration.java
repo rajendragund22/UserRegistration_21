4cc776aa1ce6de6a01d06d8e6face18b1b580d6d
RajendraGund <rajendragund2015@gmail.com>
2021-09-20 02:12:46 +0530

    Added : Program for the Last Name is in format or not using Test Case

--- src/main/java/com/bridgelabz/exception/UserRegistration.java
+++ src/main/java/com/bridgelabz/exception/UserRegistration.java
@@ -10,4 +10,10 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
+    public boolean lastName(String lastName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
 }
--- src/test/java/com/bridgelabz/exception/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/exception/UserRegistrationTest.java
@@ -19,4 +19,17 @@ public class UserRegistrationTest {
         boolean result = userRegistration.firstName("rajendra");
         Assert.assertEquals(false, result);
     }
+
+    @Test
+    public void givenLastName_WhenInFormat_ShouldReturnTrue() {
+        boolean result = userRegistration.lastName("Gund");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenOurOfFormat_ShouldReturnFalse() {
+        boolean result = userRegistration.lastName("gund");
+        Assert.assertEquals(false, result);
+
+    }
 }
